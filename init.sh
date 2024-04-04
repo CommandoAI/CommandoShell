8965a68b46a89b442d8941fa9a071efd23475fce
Elias Absawy <72975337+SatoshiMiracle@users.noreply.github.com>
2024-04-04 19:12:30 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,13 +2,11 @@
 
 os_type=$(uname)
 
-# Move main.sh to /usr/local/bin
+sudo mkdir -p /usr/local/bin
 sudo cp main.sh /usr/local/bin
 
-# Make main.sh executable
 sudo chmod +x /usr/local/bin/main.sh
 
-# Add "commando" function to ~/.bashrc
 if [[ $os_type == "Linux" ]]; then
   echo 'commando() { /usr/local/bin/main.sh "$@"; }' >> ~/.bashrc
   source ~/.bashrc
